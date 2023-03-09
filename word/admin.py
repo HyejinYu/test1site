d61895d9173844ec6850c814832e9497b20d49fe
Hyejin Yu <yuske1026@naver.com>
2023-03-09 17:37:29 +0900

    관리자 페이지 검색 기능 추가 전체 시험보기 관리자 검색 가능하도록

--- word/admin.py
+++ word/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Book, Day, Word, Test, Row
 
+
+
+class TestAdmin(admin.ModelAdmin):
+    search_fields = ['create_user__nickname']
+
+
+class WordAdmin(admin.ModelAdmin):
+    search_fields = ['english', 'korean']
+
+
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Day)
-admin.site.register(Word)
-admin.site.register(Test)
+admin.site.register(Word, WordAdmin)
+admin.site.register(Test, TestAdmin)
 admin.site.register(Row)
